cac352102d31d6e59cd81274425e7b4441c0d0f9
Elioenai Ferrari <elioenaiferrari@gmail.com>
2020-06-23 14:18:39 -0300

    update

--- lib/backend_web/controllers/enem_controller.ex
+++ lib/backend_web/controllers/enem_controller.ex
@@ -13,4 +13,37 @@ defmodule BackendWeb.EnemController do
     enem = EnemModel.show(id)
     render(conn, "show.json", enem: enem)
   end
+
+  def update(conn, %{"id" => id, "enem" => attrs}) do
+    enem = EnemModel.show(id)
+
+    with {
+           :ok,
+           %EnemSchema{} = enem
+         } <- EnemModel.update(enem, attrs) do
+      render(conn, "show.json", enem: enem)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    enem = EnemModel.show(id)
+
+    with {
+           :ok,
+           %EnemSchema{}
+         } <- EnemModel.delete(enem) do
+      render(conn, :no_content, "")
+    end
+  end
+
+  def create(conn, %{"enem" => attrs}) do
+    with {
+           :ok,
+           %EnemSchema{} = attrs
+         } <- EnemModel.create(attrs) do
+      conn
+      |> put_status(:created)
+      |> render("show.json", enem: attrs)
+    end
+  end
 end
